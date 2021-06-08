106b9bbd29f7afbf4fb03481b51b8eb456c10978
Keir Ferguson <keir_ferguson@hotmail.com>
2021-06-08 18:23:13 +0100

    End of Tuesday

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.markdown('Hope all goes well!')
+
+components.html(
+    '''
+        <iframe src="https://open.spotify.com/embed/playlist/7xOHp3ZlSBJNJOgsQwF85S" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    ''',
+    height=600
+)
